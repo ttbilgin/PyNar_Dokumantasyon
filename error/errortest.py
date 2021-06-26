f9436494f3a903abce4d05bb0982d6129ca107d2
ttbilgin <ttbilgin@gmail.com>
2021-06-26 16:06:08 +0300

    Create errortest.py

---
+++ error/errortest.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize
+from playsound import playsound # ses için 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(300, 200))    
+        self.setWindowTitle("PyQt button example - pythonprogramminglanguage.com") 
+
+        pybutton = QPushButton('Click me', self)
+        pybutton.clicked.connect(self.clickMethod)
+        pybutton.resize(100,32)
+        pybutton.move(50, 50)        
+
+    def clickMethod(self):
+        playsound('error.mp3',False)#async çalması için False yaptık.
+        print('Clicked Pyqt button.')
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
