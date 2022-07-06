0b6006f88d5a1e9a5fe7cf937603bac55f1a5191
Fatih Ateş <51250249+fatiiates@users.noreply.github.com>
2022-07-06 21:21:23 +0300

    Update pynarrestore.sh

--- PyNarBackupScript/pynarrestore.sh
+++ PyNarBackupScript/pynarrestore.sh
@@ -1,1 +1,46 @@
+#!/bin/sh
+# $1=veritabanı kullanıcı adı, $2= veritabanı şifresi, $3=tar dosyasının yolu
 
+# root dizinine geçiliyor
+cd /
+
+echo "Tar dosyası çıkarılıyor..."
+
+# tar dosyası çıkarılıyor
+tar -xf $3
+
+if [ $? -eq 0 ] 
+then 
+  echo "Tar dosyasını çıkarma başarılı." 
+else 
+  echo "Tar dosyasını çıkarma başarısız oldu!"
+  exit
+fi
+
+echo "Veri tabanı oluşturuluyor..."
+
+# restfulapi veritabanı oluşturuluyor
+mysql -u $1 -p$2 -e "create database restfulapi"
+
+if [ $? -eq 0 ] 
+then 
+  echo "Veri tabanı oluşturuldu." 
+else 
+  echo "Veri tabanı oluşturma başarısız oldu!"
+  exit
+fi
+
+echo "Veritabanına script ile veriler yükleniyor..."
+
+# restfulapi veritabanı üzerine şema ve veriler yerleştiriliyor
+mysql -u $1 -p$2 restfulapi < /var/www/dump.sql
+
+if [ $? -eq 0 ] 
+then 
+  echo "Veri tabanına veriler başarıyla aktarıldı." 
+else 
+  echo "Veri tabanına veriler aktarılamadı!"
+  exit
+fi
+
+echo "Restorasyon tamamlandı."
