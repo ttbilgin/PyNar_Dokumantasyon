13bdb02ed2c248c92ebe29478f69c4cd11d0d3cd
Fatih Ateş <51250249+fatiiates@users.noreply.github.com>
2022-08-04 15:01:44 +0300

    Update pynarrestore.sh

--- PyNarBackupScript/pynarrestore.sh
+++ PyNarBackupScript/pynarrestore.sh
@@ -1,13 +1,42 @@
-#!/bin/sh
-# $1=veritabanı kullanıcı adı, $2= veritabanı şifresi, $3=tar dosyasının yolu
+#!/bin/bash
+# $1=veritabanı kullanıcı adı, $2= veritabanı şifresi
 
-# root dizinine geçiliyor
+apt -y update && apt -y upgrade
+
+export DEBIAN_FRONTEND=noninteractive 
+
+packagelist=(
+    python3-pip
+    python3
+    apache2
+    libssl-dev
+    pkg-config
+    libonig-dev
+)
+
+apt-get install -y ${packagelist[@]}
+
+systemctl enable apache2.service
+
+a2enmod rewrite
+
+systemctl restart apache2.service
+
+apt-get install -y mysql-server
+
+mysql_secure_installation
+
 cd /
+
+usermod -aG sudo www-data
+usermod -aG root www-data
 
 echo "Tar dosyası çıkarılıyor..."
 
 # tar dosyası çıkarılıyor
 tar -xf $3
+
+pip3 install -r /var/www/pynar.org/api/scripts/requirements.txt
 
 if [ $? -eq 0 ] 
 then 
@@ -44,3 +73,17 @@ else
 fi
 
 echo "Restorasyon tamamlandı."
+
+echo "SERTİFİKASYON İŞLEMLERİ BAŞLATILIYOR..."
+
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt install python3 python3-venv libaugeas0
+
+sudo python3 -m venv /opt/certbot/ && sudo /opt/certbot/bin/pip install --upgrade pip
+
+sudo /opt/certbot/bin/pip install certbot certbot-apache
+
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+
+sudo certbot --apache -d pynar.org -d www.pynar.org
